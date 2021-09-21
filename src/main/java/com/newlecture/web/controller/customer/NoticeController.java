fc8b42939fc7e3189f300d3288ed6552d502bd73
qswazz <89292521+qswazz@users.noreply.github.com>
2021-09-21 13:12:30 +0900

    Feat : Study Spring body :     1. QueryString 입력하기.

--- src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -3,9 +3,12 @@ package com.newlecture.web.controller.customer;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
@@ -21,8 +24,12 @@ public class NoticeController
 	
 
 	@RequestMapping("list")
-	public String list() throws ClassNotFoundException, SQLException
+	public String list(@RequestParam(name="p", defaultValue="1") String page) throws ClassNotFoundException, SQLException
 	{
+		//String p = request.getParameter("p");
+		System.out.println("page: " + page);
+		
+		
 		List<Notice> list = noticeService.getList(1, "TITLE", "");
 		
 		return "notice.list";
